17ac999d5ff34a39ab937ed9bdcf1137fe15a1e9
Yra <yrapilipko2010@gmail.com>
2026-04-13 20:55:20 +0300

    reload

---
+++ Assets/NETROGAT/Script/CameraOpenDoor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace CameraDoorScript
+{
+    public class CameraOpenDoor : MonoBehaviour
+    {
+        public float DistanceOpen = 3f;
+        public GameObject interactionText; // Лучше давать понятные имена
+
+        void Update()
+        {
+            RaycastHit hit;
+            // Пускаем луч из позиции камеры вперед
+            if (Physics.Raycast(transform.position, transform.forward, out hit, DistanceOpen))
+            {
+                // Пытаемся получить компонент двери один раз за попадание луча
+                var door = hit.transform.GetComponent<DoorScript.Door>();
+
+                if (door != null)
+                {
+                    if (interactionText != null) interactionText.SetActive(true);
+
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        door.OpenDoor();
+                    }
+                }
+                else
+                {
+                    HideText();
+                }
+            }
+            else
+            {
+                HideText();
+            }
+        }
+
+        private void HideText()
+        {
+            if (interactionText != null && interactionText.activeSelf)
+            {
+                interactionText.SetActive(false);
+            }
+        }
+    }
+}
